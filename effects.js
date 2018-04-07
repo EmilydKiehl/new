a3389db5dba6a6aeadb2fbe89fdf527aa6b24024
Emily Kiehl <emilydkiehl@gmail.com>
2018-04-06 22:28:11 -0400

    Update effects.js

--- effects.js
+++ effects.js
@@ -65,7 +65,7 @@ $('document').ready(function(){
 
 	$('#yes').click(function(){
 		$('.balloon-border').animate({top:-500},8000);
-		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
+		$('#b4,#b5').addClass('balloons-rotate-behaviour-one');
 		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
 		// $('#b3').addClass('balloons-rotate-behaviour-two');
 		// $('#b4').addClass('balloons-rotate-behaviour-one');
