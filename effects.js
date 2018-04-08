84eb7d9e6ad93d0fa1392269efe2140139c62cf8
Emily Kiehl <emilydkiehl@gmail.com>
2018-04-07 20:05:31 -0400

    Update effects.js

--- effects.js
+++ effects.js
@@ -7,13 +7,13 @@ $('document').ready(function(){
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-			$('#b11').animate({top:240, left: vw-350},500);
-			$('#b22').animate({top:240, left: vw-250},500);
-			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+150},500);
-			$('#b77').animate({top:240, left: vw+250},500);
+			$('#b1').animate({top:240, left: vw-350},500);
+			$('#b2').animate({top:240, left: vw-250},500);
+			$('#b3').animate({top:240, left: vw-150},500);
+			$('#b4').animate({top:240, left: vw-50},500);
+			$('#b5').animate({top:240, left: vw+50},500);
+			$('#b6').animate({top:240, left: vw+150},500);
+			$('#b7').animate({top:240, left: vw+250},500);
 		});
 	
 	function loopOne() {
@@ -67,7 +67,7 @@ $('document').ready(function(){
 		});
 	}
 
-	$('#balloons_flying').click(function(){
+	$('#clickme').click(function(){
 		$('.balloon-border').animate({top:-500},8000);
 		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
 		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
@@ -85,6 +85,7 @@ $('document').ready(function(){
 		loopSeven();
 		
 		$(this).fadeOut('slow').delay(5000).promise().done(function(){
-			$('#cake_fadein').fadeIn('slow');
+			$('#yes').fadeIn('slow');
+			$('#absolutely').fadeIn('slow');
 		});
 	});	
